ba62b8389ee0177957b7aed4111b8a2d9ac9b7e4
Yeris <heighteugene@gmail.com>
2020-11-25 23:17:05 +0200

    Добавьте файлы проекта.

---
+++ Solid_3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+interface IShape
+{
+    int GetArea();
+}
+
+class Rectangle : IShape
+{
+    public virtual int Width { get; set; }
+    public virtual int Height { get; set; }
+    public int GetArea()
+    {
+        return Width * Height;
+    }
+}
+
+class Square : IShape
+{
+    public virtual int Side { get; set; }
+
+    public int GetArea()
+    {
+        return Side * Side;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Square square = new Square();
+        square.Side = 5;
+
+        Console.WriteLine(square.GetArea());
+        Console.ReadKey();
+    }
+}
---
+++ Solid_4/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+/*Даний інтерфейс поганий тим, що він включає занадто багато методів.
+ А що, якщо наш клас товарів не може мати знижок або промокодом, або для нього немає сенсу встановлювати матеріал з 
+ якого зроблений (наприклад, для книг). Таким чином, щоб не реалізовувати в кожному класі невикористовувані в ньому методи, краще 
+розбити інтерфейс на кілька дрібних і кожним конкретним класом реалізовувати потрібні інтерфейси.
+Перепишіть, розбивши інтерфейс на декілька інтерфейсів, керуючись принципом розділення інтерфейсу. 
+Опишіть класи книжки (розмір та колір не потрібні, але потрібна ціна та знижки) та верхній одяг (колір, розмір, ціна знижка),
+які реалізують притаманні їм інтерфейси.*/
+
+interface IPriceItem
+{
+    void SetPrice(double price);
+}
+
+interface ISizeItem
+{
+    void SetSize(byte size);
+}
+
+interface IDiscountItem
+{
+   void ApplyDiscount(String discount);
+}
+
+interface IPromoItem
+{
+    void ApplyPromocode(String promocode);
+}
+
+interface IColorItem
+{
+    void SetColor(byte color);
+}
+
+class Book : IPriceItem, IDiscountItem, IPromoItem
+{
+    public void SetPrice(double price) {    }
+
+    public void ApplyDiscount(String discount) {    }
+    public void ApplyPromocode(String promocode) {    }
+
+}
+
+class OuterWear : IColorItem, ISizeItem, 
+                  IPromoItem, IDiscountItem, IPriceItem
+{
+    public void SetPrice(double price) {    }
+
+    public void ApplyDiscount(String discount) {    }
+    public void ApplyPromocode(String promocode) {    }
+
+    public void SetSize(byte size) {    }
+
+    public void SetColor(byte color) {    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+       
+        Console.ReadKey();
+    }
+}
